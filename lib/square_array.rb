7d227dd4088d0516a89b01169fd7b155b2f93fc6
Clara Yip <clara.yip1@gmail.com>
2019-06-24 17:55:39 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -2,7 +2,7 @@ def square_array(ary)
   new_array = []
   counter = 0 
   while counter < ary.size do
-    squared << (ary[counter] ** 2)
+    new_array << (ary[counter] ** 2)
     counter += 1 
   end
   return new_array
